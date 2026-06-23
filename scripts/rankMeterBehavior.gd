cdc74705402e2ef0c06bc392cf9d55e1f783f7fb
Basarin <msoro48@gmail.com>
2026-06-23 23:27:20 +0200

    fix res and updating meter

--- scripts/rankMeterBehavior.gd
+++ scripts/rankMeterBehavior.gd
@@ -1,16 +1,23 @@
-extends Sprite2D
+extends Node2D
 
-@onready var perfect_area: Area2D = $PerfectArea
-@onready var good_area: Area2D = $GoodArea
-@onready var ok_area: Area2D = $OkArea
+@onready var perfect_area: Area2D = $RankMeterSprite/PerfectArea
+@onready var good_area: Area2D = $RankMeterSprite/GoodArea
+@onready var ok_area: Area2D = $RankMeterSprite/OkArea
 
-@onready var meter: Area2D = $Meter
+@onready var visibleMeter: ProgressBar = $RankMeterSprite/ProgressBar
 
 @onready var pallina: RigidBody2D = $"../Pallina"
 
+@onready var SFXplayer: AudioStreamPlayer = $"../../SoundEffectsPlayer"
+
+const SFXPerfect = preload("res://assets/sounds/sfx/Hit/SFXHit_Perf.wav")
+const SFXGood = preload("res://assets/sounds/sfx/Hit/SFXHit_Good.wav")
+const SFXOk = preload("res://assets/sounds/sfx/Hit/SFXHit_Ok.wav")
+const SFXMiss = preload("res://assets/sounds/sfx/Hit/SFXHit_Miss.wav")
+
 signal endMinigame
 
-var roundStarted = true
+var roundStarted = false
 var calculating = false
 var mgscore: int
 var count: int
@@ -35,24 +42,40 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed() and not event.is_echo():
 		if event.keycode == KEY_SPACE:
 			calculating = true
+			var hitScore = 0
 			if roundStarted:
 				count += 1
 				if activeHitbox["PerfectArea"]:
 					print("perfect")
-					mgscore += 100
+					SFXplayer.stream = SFXPerfect
+					hitScore = 100
 				elif activeHitbox["GoodArea"]:
 					print("good")
-					mgscore += 75
+					SFXplayer.stream = SFXGood
+					hitScore = 75
 				elif activeHitbox["OkArea"]:
 					print("ok")
-					mgscore += 50
+					SFXplayer.stream = SFXGood
+					hitScore = 50
 				else:
 					print("miss")
-					mgscore += 25
+					SFXplayer.stream = SFXMiss
+					hitScore = 25
+				SFXplayer.play()
+				mgscore += hitScore
+				updateMeter(mgscore)
 			if count == 5:
 				endMinigame.emit(mgscore)
 				resetvalues()				
 			calculating = false	
+
+func updateMeter(hitScore: int):
+	var updateBar = create_tween()
+	
+	updateBar.tween_property(visibleMeter, "value", mgscore, 0.8)\
+		.set_trans(Tween.TRANS_QUAD)\
+		.set_ease(Tween.EASE_OUT)
+	pass
 
 func resetvalues():
 	roundStarted = false
--- scripts/roundManager.gd
+++ scripts/roundManager.gd
@@ -3,11 +3,16 @@ extends Node2D
 # Variabili 1a Fase
 @export var board: Node2D
 @export var boardMovement: int
+@onready var sfxPlayer = $Camera2D/SoundEffectsPlayer
+
+const SFX_board_in = preload("res://assets/sounds/sfx/Board/BoardSFX_enter3.wav")
+const SFX_board_out = preload("res://assets/sounds/sfx/Board/BoardSFX_exit3.wav")
+
 signal clearFiches
-signal startMinigame
+signal startMinigame 
 signal finalDestination
 
-var roundScore = {}
+var roundScore = {}    
 
 
 # Called when the node enters the scene tree for the first time.
@@ -24,9 +29,12 @@ func roundHandler():
 	# Fase 1: Faccio scendere la board
 	print("easeIn")
 	var mostraBoard: Tween = create_tween()
+	sfxPlayer.stream = SFX_board_in
+	mostraBoard.tween_callback(sfxPlayer.play)
 	mostraBoard.tween_property(board, "position:y", board.position.y + boardMovement, 0.6)\
 	.set_trans(Tween.TRANS_SPRING)\
-	.set_ease(Tween.EASE_OUT)	
+	.set_ease(Tween.EASE_OUT) 
+	
 	# Fine Fase 1:
 	# sul segnale dalla BoardBehavior (dopo targetSelected)  
 	# eseguo il tween per spostare la board
@@ -36,10 +44,13 @@ func roundHandler():
 	calcScore(args[0], args[1])
 	set_meta("target", args[0])
 	var rimuoviBoard: Tween = create_tween()
+	sfxPlayer.stream = SFX_board_out
+	rimuoviBoard.tween_callback(sfxPlayer.play)
 	rimuoviBoard.tween_property(board, "position:y", board.position.y - boardMovement, 0.6)\
 		.set_trans(Tween.TRANS_SPRING)\
 		.set_ease(Tween.EASE_IN)
-		
+	
+
 	startMinigame.emit()
 	pass # Replace with function body.
 
@@ -84,5 +95,6 @@ func _process(delta: float) -> void:
 func _on_rank_meter_end_minigame(score: int) -> void:
 	if has_meta("target"):
 		var goToTarget = get_meta("target")
+		print(goToTarget)
 		finalDestination.emit(goToTarget)
 	pass # Replace with function body.
